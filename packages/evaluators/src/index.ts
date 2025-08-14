965b39eee4cf3dd86fceae52c569230d08569586
Mark Peter Fejes <markpeter@quantum-intermission.space>
2025-08-14 16:26:32 +0200

    chore(evaluators): remove unused evaluators file and clean up exports

--- packages/evaluators/src/index.ts
+++ packages/evaluators/src/index.ts
@@ -1,3 +1,2 @@
 export * from "./lib/cosine-similarity-evaluator.js";
-export * from "./lib/evaluators.js";
 export * from "./lib/vector-utils.js";
--- packages/evaluators/src/lib/evaluators.ts
+++
@@ -1,3 +0,0 @@
-export function evaluators(): string {
-  return "evaluators";
-}
